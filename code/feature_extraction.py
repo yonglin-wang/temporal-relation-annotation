aad22b38f12ce63f3209fd12941704002ecc447b
Xy-Lu <loewilu@brandeis.edu>
2020-05-04 16:01:09 -0400

    Update feature_extraction.py

--- code/feature_extraction.py
+++ code/feature_extraction.py
@@ -11,14 +11,25 @@ from Event import Event
 import collections
 import numpy as np
 # Event(span, verb, pos, text)
-
+TAGS_INDEX = collections.defaultdict(int)
 TAGS = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS', 'LS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS', 'PDT', 'POS', 'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'TO', 'UH', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'WDT', 'WP', 'WP$', 'WRB']
+for i, tag in enumerate(TAGS):
+    TAGS_INDEX[tag] = i
+TAGS_LEN = len(TAGS)
+MODALS_INDEX = collections.defaultdict(int)
 MODALS = ['will', 'would', 'can', 'could', 'may', 'might']
+for i, modal in enumerate(MODALS):
+    MODALS_INDEX[modal] = i
+MODALS_LEN = len(MODALS)
+TEMPS_INDEX = collections.defaultdict(int)
 TEMPS = ['before', 'after', 'since']
+for i, temp in enumerate(TEMPS):
+    TEMPS_INDEX[temp] = i
+TEMPS_LEN = len(TEMPS)
 
 def extract_pos_tag(text, e1, e2):
     """
-    return a list of part-of-speech (POS) tags from each individual verb and from its neighboring three words.
+    return count vector of a list of part-of-speech (POS) tags from each individual verb and from its neighboring three words.
     """
     tokens = word_tokenize(text)
     tagged_words = pos_tag(tokens)
@@ -29,7 +40,13 @@ def extract_pos_tag(text, e1, e2):
         tags_after = tagged_words[idx+1 : idx+4 if idx+4<len(tokens) else len(tokens)]
         pos_list.extend([tag for _, tag in tags_before]) # tags before the verb
         pos_list.extend([tag for _, tag in tags_after]) # tags after the verb
-    return pos_list
+    
+    res = np.zeros((1,TAGS_LEN))
+    counter = collections.Counter(pos_list)
+    for pos in pos_list:
+        idx = TAGS_INDEX[pos]
+        res[0, idx] = counter[pos]
+    return res[0]
 
 def extract_str_distance(text, e1, e2):
     """
@@ -39,29 +56,41 @@ def extract_str_distance(text, e1, e2):
 
 def extract_modal_verbs(text, e1, e2):
     """
-    return a list of the modal verbs between the event mention (i.e., will, would, can, could, may and might).
+    return count vector of a list of the modal verbs between the event mention (i.e., will, would, can, could, may and might).
     """
-    res = []
+    tmp = []
     modals = set(MODALS)
     tokens = word_tokenize(text)
     intervals = tokens[e1.get_index()+1: e2.get_index()]
     for word in intervals:
         if word in modals:
-            res.append(word)
-    return res
+            tmp.append(word)
+            
+    res = np.zeros((1,MODALS_LEN))
+    counter = collections.Counter(tmp)
+    for mod in tmp:
+        idx = MODALS_INDEX[mod]
+        res[0, idx] = counter[mod]
+    return res[0]
 
 def extract_temp_connectives(text, e1, e2):
     """
-    return a list of temporal connectives between the event mentions (e.g., before, after and since)
+    return count vector of a list of temporal connectives between the event mentions (e.g., before, after and since)
     """
-    res = []
+    tmp = []
     temps = set(TEMPS)
     tokens = word_tokenize(text)
     intervals = tokens[e1.get_index()+1: e2.get_index()]
     for word in intervals:
         if word in temps:
-            res.append(word)
-    return res
+            tmp.append(word)
+            
+    res = np.zeros((1,TEMPS_LEN))
+    counter = collections.Counter(tmp)
+    for t in tmp:
+        idx = TEMPS_INDEX[t]
+        res[0, idx] = counter[t]
+    return res[0]
 
 def have_common_syn(e1, e2):
     """
@@ -80,10 +109,11 @@ def have_common_der_form(e1, e2):
 if __name__ == "__main__":
 
     data = pd.read_csv('./data/gold_standard.csv')
+    print(len(data))
     df = pd.DataFrame(data, columns= ['text',"fromSpan","fromVerb", "fromPOS", "toSpan", "toVerb","toPOS"])
-    row = 75
+    row = 0
     text = df.loc[row,'text']
     e1 = Event(df.loc[row,'fromSpan'], df.loc[row,'fromVerb'], df.loc[row,'fromPOS'], text)
     e2 = Event(df.loc[row,'toSpan'], df.loc[row,'toVerb'], df.loc[row,'toPOS'], text)
-    print(extract_pos_tag(text, e1, e2))
+    print(extract_temp_connectives(text, e1, e2))
     print(have_common_der_form(e1, e2))
