1e75ab32a5b6a25de9c7f1461b620fa3741c101c
Yonglin Wang <yonlinw@brandeis.edu>
2020-05-04 10:10:35 -0400

    Update Event.py

--- code/Event.py
+++ code/Event.py
@@ -6,30 +6,28 @@ event object for representing a verb
 import nltk
 # nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet 
+from nltk.corpus import wordnet as wn
 
+# valid_pos = {"VB", "VBD", "VBG", "VBN", "VBP", "VBZ"} # for validation if needed
 
 class Event:
 
-    def __init__(self, xml_entry):
+    def __init__(self, verb_id, start, text, pos):
         """
-        assume xml_entry has the format of
-        id="V1" spans="7~14" text="reached" category="VBN"
+        inputs are all text strings
         """
-        self.verb_id, self.start, self.text, self.pos = self.get_id_start_text_cat(xml_entry)
-        self.synset = None
-        self.derivational_form = None
-
-    
-
-    def get_id_start_text_cat(self, xml_entry):
-        pass
+        self.verb_id, self.start, self.text, self.pos = verb_id, int(start), text, pos
+        self.synset = self.set_synset(text)
+        self.derivational_form = self.set_derivational_form(text)
 
     def get_verb_id(self):
         return self.verb_id
 
     def get_start_position(self):
-        return self.start
+        return int(self.start)
+
+    def get_end_position(self):
+        return int(self.start) + len(self.text)
 
     def get_verb_text(self):
         return self.text
@@ -44,9 +42,22 @@ class Event:
         return self.derivational_form
 
     def set_synset(self, word):
-        self.synset = set(l.name() for syn in wordnet.synsets(word) for l in syn.lemmas())
+        return set(l.name() for syn in wn.synsets(word) for l in syn.lemmas())  # return set() or None if empty?
 
     def set_derivational_form(self, word):
         lemmatizer = WordNetLemmatizer()
-        self.derivational_form = lemmatizer.lemmatize(word)
+        return lemmatizer.lemmatize(word)
 
+
+if __name__ == "__main__":
+
+    test_words = {
+                    "find",
+                    "asefajiog"
+                  }  # change this set to see how diff words perform
+
+    for w in test_words:
+        e = Event("V3", "2", w, "VBG")
+        print("Synsets for word \'%s\': " % e.get_verb_text())
+        print(e.get_synset())
+        print("Derivational form for word \'%s\': %s" % (e.get_verb_text(), e.get_derivational_form()))
