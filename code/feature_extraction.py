e71e65339dfedbc78be3dff029f56ac504a26f59
Xy-Lu <loewilu@brandeis.edu>
2020-05-04 13:00:31 -0400

    Update feature_extraction.py

--- code/feature_extraction.py
+++ code/feature_extraction.py
@@ -8,8 +8,13 @@ import pandas as pd
 from nltk.tokenize import word_tokenize 
 from nltk import pos_tag
 from Event import Event
-
+import collections
+import numpy as np
 # Event(span, verb, pos, text)
+
+TAGS = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS', 'LS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS', 'PDT', 'POS', 'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'TO', 'UH', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'WDT', 'WP', 'WP$', 'WRB']
+MODALS = ['will', 'would', 'can', 'could', 'may', 'might']
+TEMPS = ['before', 'after', 'since']
 
 def extract_pos_tag(text, e1, e2):
     """
@@ -20,40 +25,65 @@ def extract_pos_tag(text, e1, e2):
     idx1, idx2 = e1.get_index(), e2.get_index()
     pos_list = []
     for idx in [idx1, idx2]:
-        pos_list.append(tagged_words[idx-3 if idx-3 >=0 else 0 : idx]) # before the verb
-        pos_list.append(tagged_words[idx+1 : idx+4 if idx+4<len(tokens) else len(tokens)]) # after the verb
-
+        tags_before = tagged_words[idx-3 if idx-3 >=0 else 0 : idx]
+        tags_after = tagged_words[idx+1 : idx+4 if idx+4<len(tokens) else len(tokens)]
+        pos_list.extend([tag for _, tag in tags_before]) # tags before the verb
+        pos_list.extend([tag for _, tag in tags_after]) # tags after the verb
     return pos_list
 
 def extract_str_distance(text, e1, e2):
     """
     return an int of the distance between them in terms of the number of tokens.
     """
-    pass
+    return e2.get_index() - e1.get_index()
 
 def extract_modal_verbs(text, e1, e2):
     """
     return a list of the modal verbs between the event mention (i.e., will, would, can, could, may and might).
     """
-    pass
+    res = []
+    modals = set(MODALS)
+    tokens = word_tokenize(text)
+    intervals = tokens[e1.get_index()+1: e2.get_index()]
+    for word in intervals:
+        if word in modals:
+            res.append(word)
+    return res
 
 def extract_temp_connectives(text, e1, e2):
     """
     return a list of temporal connectives between the event mentions (e.g., before, after and since)
     """
-    pass
+    res = []
+    temps = set(TEMPS)
+    tokens = word_tokenize(text)
+    intervals = tokens[e1.get_index()+1: e2.get_index()]
+    for word in intervals:
+        if word in temps:
+            res.append(word)
+    return res
 
 def have_common_syn(e1, e2):
     """
-    return a boolean of whether the two verbs have a common synonym from their synsets in WordNet (Fellbaum, 1998)
+    return either 0 or 1 of whether the two verbs have a common synonym from their synsets in WordNet (Fellbaum, 1998)
     """
-    pass
+    syns1, syns2 = e1.get_synset(), e2.get_synset()
+    return 0 if not len(syns1.intersection(syns2)) else 1
 
 def have_common_der_form(e1, e2):
     """
-    return a boolean of whether the input event mentions have a common derivational form derived from WordNet.
+    return either 0 or 1 of whether the input event mentions have a common derivational form derived from WordNet.
     """
-    pass
+    derv1, derv2 = e1.get_derivational_form(), e2.get_derivational_form()
+    return 1 if derv1 == derv2 else 0
 
 if __name__ == "__main__":
-    pass
+
+    data = pd.read_csv('./data/gold_standard.csv')
+    df = pd.DataFrame(data, columns= ['text',"fromSpan","fromVerb", "fromPOS", "toSpan", "toVerb","toPOS"])
+    row = 75
+    text = df.loc[row,'text']
+    e1 = Event(df.loc[row,'fromSpan'], df.loc[row,'fromVerb'], df.loc[row,'fromPOS'], text)
+    e2 = Event(df.loc[row,'toSpan'], df.loc[row,'toVerb'], df.loc[row,'toPOS'], text)
+    print(extract_pos_tag(text, e1, e2))
+    print(have_common_der_form(e1, e2))
